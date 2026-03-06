91a574acfd34a14b1522dfc316fb65b6ea2050b0
pv220112478 <pv220112478@vallarta.tecmm.edu.mx>
2026-03-05 18:39:19 -0600

    Nueva regla de negocio: no descuento mayor a 1000

--- kevin/__test__/calculos.test.ts
+++ kevin/__test__/calculos.test.ts
@@ -1,4 +1,4 @@
-import { aplicarDescuento } from './calculos';
+import { aplicarDescuento } from '../calculos';
 describe('Pruebas de Nómina Tec-Café', () => {
    //prueba1
    test('Debe aplicar 15% de descuento a alumnos de Sistemas',
@@ -9,6 +9,8 @@ describe('Pruebas de Nómina Tec-Café', () => {
 //
   test('Debe lanzar error si el total es negativo', () => {
    expect(() => aplicarDescuento(-1, true)).toThrow("Total negativo");});
-
-
+//prueba no descuento +1000
+  test('no hara descuento si el total es mas de 1000', () => {
+   expect(() => aplicarDescuento(1001, true)).toThrow("mas de 1000 pesos");
+});
 });
--- kevin/calculos.ts
+++ kevin/calculos.ts
@@ -1,5 +1,6 @@
 //creacion de funcion de  flecha  gorda ( fat arrow)
 export const aplicarDescuento = (total: number, esSistemas: boolean): number => 
              { if (total < 0) throw new Error("Total negativo");
+                if (total > 1000) throw new Error("mas de 1000 pesos");
                 return esSistemas ? total * 0.85 : total; 
               };
