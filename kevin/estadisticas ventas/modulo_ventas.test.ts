e4e2e10f9821c34122d4df872a3d80f4729a70ff
pv220112478 <pv220112478@vallarta.tecmm.edu.mx>
2026-03-10 19:26:12 -0600

    1. Arreglo vacío (debe retornar 0 o error).

--- kevin/estadisticas ventas/modulo_ventas.test.ts
+++ kevin/estadisticas ventas/modulo_ventas.test.ts
@@ -1,6 +1,6 @@
 import { ObtenerPromedio } from './modulo_ventas';
 describe('Modulo de estadisticas de ventas', () => {
-    test('esta vacio',() => { 
+    test('esta vacio',() => { //Arreglo vacío (debe retornar 0 o error).
         expect(ObtenerPromedio([])).toThrow('error'); });
     
 });
