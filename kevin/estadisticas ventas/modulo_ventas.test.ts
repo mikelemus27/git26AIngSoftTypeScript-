c77536eb8dd08fc37622f9be7e93f1bf27b12ccc
pv220112478 <pv220112478@vallarta.tecmm.edu.mx>
2026-03-10 19:23:36 -0600

    creacion archivos modulo_ventas.ts y .test.ts

--- kevin/estadisticas ventas/modulo_ventas.test.ts
+++ kevin/estadisticas ventas/modulo_ventas.test.ts
@@ -1,9 +1,6 @@
 import { ObtenerPromedio } from './modulo_ventas';
 describe('Modulo de estadisticas de ventas', () => {
-    //test('esta vacio',
-          // () => { expect(ObtenerPromedio([])).toThrow('error'); });
-    test('una sola venta', () => {
-        const ventas = [85];
-        expect(ObtenerPromedio(ventas)).toBe(85);
-    });
+    test('esta vacio',() => { 
+        expect(ObtenerPromedio([])).toThrow('error'); });
+    
 });
--- kevin/estadisticas ventas/modulo_ventas.ts
+++ kevin/estadisticas ventas/modulo_ventas.ts
@@ -1,4 +1,4 @@
-export const ObtenerPromedio = (ventas: number[]): number => 
+export const ObtenerPromedio = (ventas: number[]): number => //variable tipo cadena
              { if (ventas.length === 0) throw new Error("ningun registro");
               const suma = ventas.reduce((acc, precio) => acc + precio, 0);
               return suma / ventas.length;
