df1a74146b8e223d652f8ca1ddeab6a3b3c162d2
pv220112478 <pv220112478@vallarta.tecmm.edu.mx>
2026-03-10 19:27:22 -0600

    2. Arreglo con una sola venta.

--- kevin/estadisticas ventas/modulo_ventas.test.ts
+++ kevin/estadisticas ventas/modulo_ventas.test.ts
@@ -2,5 +2,8 @@ import { ObtenerPromedio } from './modulo_ventas';
 describe('Modulo de estadisticas de ventas', () => {
     test('esta vacio',() => { //Arreglo vacío (debe retornar 0 o error).
         expect(ObtenerPromedio([])).toThrow('error'); });
-    
+    test('una sola venta', () => { //Arreglo con una sola venta.
+        const ventas = [85];
+        expect(ObtenerPromedio(ventas)).toBe(85);
+    });
 });
