1453e5b4f9064e4fd4300df465a8bc231bacc799
pv220112478 <pv220112478@vallarta.tecmm.edu.mx>
2026-03-11 23:51:56 -0600

    4 ventas valor 0.

--- kevin/estadisticas ventas/modulo_ventas.test.ts
+++ kevin/estadisticas ventas/modulo_ventas.test.ts
@@ -1,13 +1,19 @@
 import { ObtenerPromedio } from './modulo_ventas';
 describe('Modulo de estadisticas de ventas', () => {
-    test('esta vacio',() => { //Arreglo vacío (debe retornar 0 o error).
-        expect(ObtenerPromedio([])).toThrow('error'); });
-    test('una sola venta', () => { //Arreglo con una sola venta.
-        const ventas = [85];
+    
+    test('Arreglo vacio', () => {
+    expect(() => ObtenerPromedio([])).toThrow('No hay ninguna venta');
+    });
+    test('Arreglo con una sola venta.', () => { 
+        const ventas : number[] = [85];
         expect(ObtenerPromedio(ventas)).toBe(85);
     });
-    test('varias ventas con decimales', () => { //Ventas con decimales.
-        const ventas = [10.5, 20.5, 30.0];
+    test('Ventas con decimales.', () => { 
+        const ventas : number[] = [10.5, 20.5, 30.0];
         expect(ObtenerPromedio(ventas)).toBeCloseTo(20.33, 2);
+    });
+    test('Ventas con valor 0', () => { 
+        const ventas: number[] = [0];
+        expect(ObtenerPromedio(ventas)).toBe(0);
     });
 });
--- kevin/estadisticas ventas/modulo_ventas.ts
+++ kevin/estadisticas ventas/modulo_ventas.ts
@@ -1,5 +1,5 @@
 export const ObtenerPromedio = (ventas: number[]): number => //variable tipo cadena
-             { if (ventas.length === 0) throw new Error("ningun registro");
+             { if(ventas.length === 0) throw new Error('No hay ninguna venta');
               const suma = ventas.reduce((acc, precio) => acc + precio, 0);
               return suma / ventas.length;
               };
