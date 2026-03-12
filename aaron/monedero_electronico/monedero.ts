4e8b998763d34495d29a6cc4f7f2262e6ea05c79
3ao8 <aaronvipe@gmail.com>
2026-03-12 17:39:57 -0600

    Creación de primer codigo en monedero.ts

--- aaron/monedero_electronico/monedero.ts
+++ aaron/monedero_electronico/monedero.ts
@@ -1,3 +1,23 @@
 // Aaron Everardo Villegas Pelayo
 // Módulo de Monedero Electrónico
 
+const SALDO_MAXIMO = 5000;
+
+export function realizarPago(saldo: number, monto: number): number {
+
+    if (saldo > SALDO_MAXIMO) {
+        throw new Error("El saldo excede el límite permitido");
+    }
+    if (monto < 0) {
+        throw new Error("El monto de pago no puede ser negativo");
+    }
+    if (monto === 0) {
+        return saldo;
+    }
+    if (monto > saldo) {
+        throw new Error("Saldo Insuficiente");
+    }
+
+    const nuevoSaldo = saldo - monto;
+    return Math.round(nuevoSaldo * 100) / 100;
+}
