90a32f4c853300f73eb890bc5098bcb14f9cdda7
3ao8 <aaronvipe@gmail.com>
2026-03-12 17:43:51 -0600

    Primer codigo para los test de la funcion  realizarpago en monedero.test.ts

---
+++ aaron/monedero_electronico/monedero.test.ts
@@ -0,0 +1,19 @@
+import { realizarPago } from "./monedero";
+
+describe("Monedero Electrónico", () => {
+
+    test("Pago menor al saldo", () => {
+        expect(realizarPago(100, 40)).toBe(60);
+    });
+
+    test("Pago exacto al saldo", () => {
+        expect(realizarPago(100, 100)).toBe(0);
+    });
+
+    test("Pago mayor al saldo", () => {
+        expect(() => realizarPago(50, 100)).toThrow("Saldo Insuficiente");
+    });
+
+
+    
+    });
