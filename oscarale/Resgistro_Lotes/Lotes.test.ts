b1449fa7c678f83ac308caf784b45bbf1b509c35
PV230112269-LANG <pv230112269@vallarta.tecmm.edu.mx>
2026-03-13 11:49:46 -0600

    actualización del proyecto

--- oscarale/Resgistro_Lotes/Lotes.test.ts
+++ oscarale/Resgistro_Lotes/Lotes.test.ts
@@ -17,40 +17,40 @@ test("1. ID válido", () => {
     expect(validarId([1,10,15])).toBe(true);
 });
 
-//test("2. ID inválido", () => {
-    //expect(validarId([0,10,15])).toBe(false);
-//});
+test("2. ID inválido", () => {
+    expect(validarId([0,10,15])).toBe(false);
+});
 
-//test("3. Días válidos", () => {
-    //expect(validarDias([1,5,15])).toBe(true);
-//});
+test("3. Días válidos", () => {
+    expect(validarDias([1,5,15])).toBe(true);
+});
 
-//test("4. Temperatura válida", () => {
-    //expect(validarTemperatura([1,5,20])).toBe(true);
-//});
+test("4. Temperatura válida", () => {
+    expect(validarTemperatura([1,5,20])).toBe(true);
+});
 
-//test("5. Lote fresco", () => {
+test("5. Lote fresco", () => {
     //expect(esFresco([1,10,15])).toBe(true);
-//});
+});
 
-//test("6. Lote vencido", () => {
-    //expect(estaVencido([1,35,15])).toBe(true);
-//});
+test("6. Lote vencido", () => {
+    expect(estaVencido([1,35,15])).toBe(true);
+});
 
-//test("7. Temperatura ideal", () => {
-    //expect(temperaturaIdeal([1,10,15])).toBe(true);
-//});
+test("7. Temperatura ideal", () => {
+    expect(temperaturaIdeal([1,10,15])).toBe(true);
+});
 
-//test("8. Estado del lote", () => {
-    //expect(estadoLote([1,20,15])).toBe("Consumir pronto");
-//});
+test("8. Estado del lote", () => {
+    expect(estadoLote([1,20,15])).toBe("Consumir pronto");
+});
 
-//test("9. Lote completamente válido", () => {
-    //expect(loteValido([2,10,15])).toBe(true);
-//});
+test("9. Lote completamente válido", () => {
+    expect(loteValido([2,10,15])).toBe(true);
+});
 
-//test("10. Resumen del lote", () => {
-    //expect(resumenLote([3,5,12])).toBe("Lote 3 - Estado: Fresco");
-//});
+test("10. Resumen del lote", () => {
+    expect(resumenLote([3,5,12])).toBe("Lote 3 - Estado: Fresco");
+});
 
 });
